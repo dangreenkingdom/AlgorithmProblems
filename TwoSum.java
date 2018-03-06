5c81f7e027a83346dd180d5acf89601b07acbb5d
dangreenkingdom <36975275+dangreenkingdom@users.noreply.github.com>
2018-03-06 20:52:37 +0800

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -4,17 +4,38 @@ Given an array of integers, return indices of the two numbers such that they add
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 */
 
-class Solution {
+class Solution1 {
     public int[] twoSum(int[] nums, int target) {
-        if (nums == null || nums.length < 2)
-            return null;
-            
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int[] ret = new int[2];
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++){
-                if (nums[i] + nums[j] == target)
-                    return new int[] {i, j};
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int x = target - nums[i];
+            if (map.containsKey(x) && (map.get(x) != i)) {
+                ret[0] = i;
+                ret[1] = map.get(x);
             }
         }
-        return null;
+        return ret;
     }
 }
+
+class Solution2 {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int[] ret = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            int x = target - nums[i];
+            if (map.containsKey(x)) {
+                ret[0] = i;
+                ret[1] = map.get(x);
+            }
+            map.put(nums[i], i);
+        }
+        return ret;
+    }
+}
+
+// 关键在想到HashMap
