f6d57002558d3706cea2435a6262b363c312a466
dangreenkingdom <36975275+dangreenkingdom@users.noreply.github.com>
2018-03-06 19:52:43 +0800

    Create TwoSum.java

---
+++ TwoSum.java
@@ -0,0 +1,20 @@
+/* 
+Description:
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+*/
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length < 2)
+            return null;
+            
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++){
+                if (nums[i] + nums[j] == target)
+                    return new int[] {i, j};
+            }
+        }
+        return null;
+    }
+}
