ae0ae2095967d848396ce2177fb958c0189fc95e
dangreenkingdom <36975275+dangreenkingdom@users.noreply.github.com>
2018-04-18 18:44:35 +0800

    Update ReverseInteger.java

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -33,3 +33,17 @@ class Solution {
         return res;
     }
 }
+
+class Solution {
+    public int reverse(int x) { // 模十取余
+        long rst = 0;
+        while (x != 0) {
+            rst = rst * 10 + x % 10;
+            x /= 10;
+            if (rst > Integer.MAX_VALUE || rst < Integer.MIN_VALUE) {
+                return 0;
+            }
+        }
+        return (int) rst;
+     }
+}
